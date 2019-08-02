6d73530ce5b74060b048d68fb3fb560a02827914
josh <questofiranon@gmail.com>
2019-08-02 10:05:49 -0700

    removes duplicate macro

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -6,26 +6,6 @@ use git2::Config;
 use std::fs;
 use structopt::StructOpt;
 
-macro_rules! grm_root {
-    () => {{
-        use git2::Config;
-
-        let config =
-            Config::open_default().expect("No git config found, do you have git installed?");
-
-        match config.get_path("grm.root") {
-            Ok(root) => root,
-            Err(_error) => match config.get_path("ghq.root") {
-                Ok(root) => root,
-                Err(_error) => {
-                    println!("grm.root not specified in git config");
-                    return;
-                }
-            },
-        }
-    }};
-}
-
 #[derive(StructOpt, Debug)]
 pub struct Get {
     /// Perform an update for an already cloned repository (roughly equivalent to `git pull --ff-only`)
